a35b6c3c3de551ec4fe73594c860027377cfef3e
Lara <laradicp@gmail.com>
2023-06-28 20:07:13 -0300

    fix script to run scripts

--- run/run-scripts.sh
+++ run/run-scripts.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-./run-heuristic.sh && ./run-lit-asc-it-heur.sh & ./run-lit-bin-heur-comb.sh & ./run-lit-bin-heur-triv.sh & ./run-lit-desc-it-comb-heur-primal.sh & ./run-lit-regular-heur-primal.sh & ./run-lit-sos1-heur-primal.sh & ./run-real.sh &
+./run-heuristic.sh && {
+    ./run-lit-asc-it-heur.sh &
+    ./run-lit-bin-heur-comb.sh &
+    ./run-lit-bin-heur-triv.sh &
+    ./run-lit-desc-it-comb-heur-primal.sh &
+    ./run-lit-regular-heur-primal.sh &
+    ./run-lit-sos1-heur-primal.sh &
+    ./run-real.sh &
+}
