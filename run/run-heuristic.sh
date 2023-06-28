6a91780bcdc19b6868d7929382cdfefc63f189c1
Lara <laradicp@gmail.com>
2023-06-28 19:32:02 -0300

    fix heuristic script

--- run/run-heuristic.sh
+++ run/run-heuristic.sh
@@ -8,5 +8,5 @@ mkdir -p results/heuristic
 
 for file in $(find instances/ -type f); do
     echo "Running $file"
-    ./max-csp.exe "$file" -heuristic -noexact > "results/literature/asc-iterative/heuristic/$(basename "$file")"
+    ./max-csp.exe "$file" -heuristic -noexact > "results/heuristic/$(basename "$file")"
 done
