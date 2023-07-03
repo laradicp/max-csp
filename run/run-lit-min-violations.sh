3b14cf1f2d0280959979e4d30171a99d81094464
Lara <laradicp@gmail.com>
2023-07-03 03:58:58 -0300

    add min violations results

--- run/run-lit-min-violations.sh
+++ run/run-lit-min-violations.sh
@@ -8,6 +8,6 @@ mkdir -p results/literature/min-violations/penalize
 
 for file in $(find instances/literature/ -type f); do
     echo "Running $file"
-    ./max-csp.exe "$file" -minviolations > "results/literature/min-violations/$(basename "$file" .in)".out
-    ./max-csp.exe "$file" -minviolations -penalize > "results/literature/min-violations/penalize/$(basename "$file" .in)".out
+    ./max-csp.exe "$file" -minviolations > "results/literature/min-violations/$(basename "$file")"
+    ./max-csp.exe "$file" -minviolations -penalize > "results/literature/min-violations/penalize/$(basename "$file")"
 done
