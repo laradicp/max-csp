eb460e4621c2b6fa5b71d13f810b300be3945db1
Lara <laradicp@gmail.com>
2023-07-06 17:19:56 -0300

    fix script

--- run/run-lit-desc-it-comb.sh
+++ run/run-lit-desc-it-comb.sh
@@ -6,7 +6,7 @@ make
 
 mkdir -p results/literature/desc-iterative/combinatorial
 
-for file in $(find instances/literature/hard/*); do
+for file in $(find instances/literature/ -type f); do
     echo "Running $file"
     ./max-csp.exe "$file" -descitsearch > "results/literature/desc-iterative/combinatorial/$(basename "$file")"
 done
