b0ee2a6d123500bdf080cc4045785bf3d214f571
Lara <laradicp@gmail.com>
2023-07-06 11:21:29 -0300

    update results

--- run/heur-tuning/run-calculate-perturbation-gaps.sh
+++ run/heur-tuning/run-calculate-perturbation-gaps.sh
@@ -2,14 +2,18 @@
 
 cd ../../results/heuristic-tuning
 
-# g++ best-bounds.cpp -o best-bounds.exe
+g++ best-bounds.cpp -o best-bounds.exe
 
 g++ calculate-perturbation-gaps.cpp -o calculate-perturbation-gaps.exe
 
-# for file in $(ls ../../instances/literature/hard/*); do
-#     echo "Calculating best bounds of file $file"
-#     ./best-bounds.exe "$(basename "$file")"
-# done
+# Clear output files before starting
+> "best-dual.txt"
+> "best-primal.txt"
+
+for file in $(ls ../../instances/literature/hard/*); do
+    echo "Calculating best bounds of file $file"
+    ./best-bounds.exe "$(basename "$file")"
+done
 
 for file in $(ls ../../instances/literature/hard/*); do
     echo "Calculating gaps of $file"
