6cbe641cb0f7ec450145e2c8c278e25e888e4153
Lara <laradicp@gmail.com>
2023-07-22 05:55:49 -0300

    compute max values for instance plot normalization

--- run/run-instance-plot-data.sh
+++ run/run-instance-plot-data.sh
@@ -7,6 +7,11 @@ g++ instance-plot-data.cpp -o instance-plot-data.exe
 # Clear output files before starting
 > "instance-plot-data.txt"
 
+for file in $(find ../instances/literature/* -type f); do
+    echo "Running $file"
+    ./instance-plot-data.exe "$file" -maxvals
+done
+
 for file in $(find literature/regular/heuristic-primal/* -type f); do
     echo "Running $file"
     ./instance-plot-data.exe "$file" >> instance-plot-data.txt
