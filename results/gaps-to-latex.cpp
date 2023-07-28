b5b7cf49718a2d92b154735af4c152cc315820a7
Lara <laradicp@gmail.com>
2023-07-28 07:43:13 -0300

    update results latex tables

--- results/gaps-to-latex.cpp
+++ results/gaps-to-latex.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <string.h>
 #include <fstream>
 #include <map>
 
@@ -43,71 +44,104 @@ GapTime readFile(string gapTimePath)
 
 string getMethodName(string method)
 {
-    if(method.find("asc") != string::npos)
+    if(method.find("regular") != string::npos)
     {
-        return "incremental";
+        return "F1";
+    }
+    if(method.find("branching") != string::npos)
+    {
+        return "F2";
+    }
+    else if(method.find("sos1") != string::npos)
+    {
+        if(method.find("asc") != string::npos)
+        {
+            return "F2$_I$";
+        }
+        else if(method.find("desc") != string::npos)
+        {
+            return "F2$_D$";
+        }
+        else
+        {
+            return "F2$_B$";
+        }
+    }
+    else if(method.find("asc") != string::npos)
+    {
+        return "I$_I$";
     }
     else if(method.find("desc") != string::npos)
     {
-        return "decremental";
+        return "I$_D$";
     }
-    else if(method.find("min-violations") != string::npos)
+    else
     {
-        return "Bautista";
+        return "I$_B$";
     }
 
     return method;
 }
 
-string initialization(string path)
+bool isInitialized(string path)
 {
     if(path.find("heur") != string::npos)
     {
-        return "initialiazed";
+        return true;
     }
     else
     {
-        return "uninitialized";
+        return false;
     }
 }
 
 int main(int argc, char** argv)
 {
+    if(argc < 2)
+    {
+        cout << "Usage: ./gaps-to-latex -<initialization>" << endl;
+        exit(1);
+    }
+
+    bool initialization = strcmp(argv[1], "-true") == 0 ? true : false;
+
     vector<string> instanceSets = {
-        // "real",
+        "real",
         "literature"
     };
-    map<string, vector<string>> methods;
-    methods["asc-iterative"].push_back("heuristic");
-    methods["asc-iterative"].push_back("combinatorial");
-    methods["binary"].push_back("heuristic-combinatorial");
-    methods["binary"].push_back("combinatorial");
-    methods["desc-iterative"].push_back("combinatorial/heuristic-primal");
-    methods["desc-iterative"].push_back("combinatorial");
-    methods["regular"].push_back("heuristic-primal");
-    methods["regular"].push_back("");
-    methods["sos1"].push_back("heuristic-primal");
-    methods["sos1"].push_back("");
-    methods["min-violations"].push_back("penalize");
+    vector<string> methods = {
+        "regular/heuristic-primal", "regular",
+        "asc-iterative/heuristic", "asc-iterative/combinatorial",
+        "desc-iterative/combinatorial/heuristic-primal", "desc-iterative/combinatorial",
+        "binary/heuristic-combinatorial", "binary/combinatorial",
+        "branching/heuristic-primal", "branching",
+        "sos1/asc-iterative/combinatorial", "sos1/asc-iterative/heuristic",
+        "sos1/desc-iterative/combinatorial/heuristic-primal", "sos1/desc-iterative/combinatorial",
+        "sos1/heuristic-primal", "sos1"
+    };
+    // methods["min-violations"].push_back("penalize");
 
-    cout << "\\hline method & initialization & \\#optimal & avg. gap (\\\%) & avg. time (s) \\\\ \\hline" << endl;
     for(int i = 0; i < instanceSets.size(); i++)
     {
-        for(auto iter = methods.begin(); iter != methods.end(); iter++)
+        cout << "\\hline method & optimal (\\\%) & avg. gap (\\\%) & avg. time (s) \\\\ \\hline" << endl;
+        for(int j = 0; j < methods.size(); j++)
         {
-            cout << "\\multirow{2}{*}{" << getMethodName(iter->first) << "}" << endl;
-            for(int j = 0; j < iter->second.size(); j++)
+            GapTime gapTime = readFile(
+                instanceSets[i] + "/" + methods[j] + "/gap-time.txt"
+            );
+
+            cout.setf(ios::fixed,ios::floatfield);
+            cout.precision(3);
+
+            if(initialization == isInitialized(methods[j]))
             {
-                GapTime gapTime = readFile(
-                    instanceSets[i] + "/" + iter->first + "/" + iter->second[j] + "/gap-time.txt"
-                );
-                cout.setf(ios::fixed,ios::floatfield);
-                cout.precision(3);
-                cout << "& " << initialization(iter->second[j]) << " & " << gapTime.optimalCount << " & " <<
-                    gapTime.gap*100/gapTime.count << " & " << gapTime.time/gapTime.count << " \\\\" << endl;
+                cout << getMethodName(methods[j]) << " & " << 
+                    gapTime.optimalCount*100.0/gapTime.count << " & " <<
+                    gapTime.gap*100/gapTime.count << " & " << gapTime.time/gapTime.count <<
+                    " \\\\" << endl;
             }
-            cout << "\\hline" << endl;
         }
+        cout << "\\hline" << endl << endl;
     }
 
     return 0;
